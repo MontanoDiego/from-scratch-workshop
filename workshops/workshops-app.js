f258b310ca64c6bd62825034517be42880e8b2cb
MontanoDiego <diego.e2.montano@gmail.com>
2022-12-02 12:50:42 -0800

    added workshops display function

--- workshops/workshops-app.js
+++ workshops/workshops-app.js
@@ -1,15 +1,44 @@
 // IMPORT
 
-import { signOutUser } from '../fetch-utils.js';
+import { getWorkshops, signOutUser } from '../fetch-utils.js';
+import { renderWorkshop } from '../render-utils.js';
 
 
 
 // DOM
 
 const signOut = document.getElementById('sign-out-link');
+const workshopsEl = document.getElementById('workshops-container');
 
 // EVENTS
 
 signOut.addEventListener('click', async () => {
     await signOutUser();
 });
+
+
+// display functions 
+
+async function displayWorkshops() {
+    // fetch families from supabase
+    const workshops = await getWorkshops();
+    // clear out the workshopsEl
+    workshopsEl.innerHTML = '';
+
+    for (let workshop of workshops) {
+        const workshopEl = renderWorkshop(workshop);
+        const participantsEl = document.createElement('ul');
+        for (let participant of workshop.participants) {
+            const participantEl = document.createElement('div');
+            participantEl.addEventListener('click', async () => {
+                console.log('clicked!');
+            });
+            participantEl.classList.add('participant');
+            participantEl.textContent = participant.name;
+
+            participantsEl.append(participantEl);
+        }
+        workshopEl.append(participantsEl);
+        workshopsEl.append(workshopEl);
+    }
+}
